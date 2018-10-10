36ea68e227bf27d3732c73d4c64c67a07d585859
AndrewKuekam17 <43419144+AndrewKuekam17@users.noreply.github.com>
2018-10-10 14:39:21 -0400

    Updeted touch and react

--- TouchAndReactAndrewK/main.lua
+++ TouchAndReactAndrewK/main.lua
@@ -9,7 +9,88 @@ display.setDefault ("background", 100/255, 89/255, 100/255)
 display.setStatusBar(display.HiddenStatusBar)
 
 -- created blue button, set its position and make it visible
-local bluebutton = display.newImageRect("Images/Fast Button Inactive@2x.png",198, 96)
+local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png",198, 96)
 blueButton.x = display.contentWidth/2
 blueButton.y = display.contentHeight/2
 blueButton.isVisible = true
+
+-- created red button, set its position and make it visible
+local redButton = display.newImageRect("Images/Fast Button Active@2x.png",198, 96)
+redButton.x = display.contentWidth/2
+redButton.y = display.contentHeight/2
+redButton.isVisible = false
+
+-- create red button, set ots position and make it invisible
+local textObject = display.newText ("Clicked!", 200, 76, nil, 50)
+textObject.x = display.contentWidth/2
+textObject.y = display. contentHeight/2
+textObject:setTextColor (1, 1, 0)
+textObject.isVisible = false
+
+-- created checkmark button, set its position and make it visible
+local checkmarkButton = display.newImageRect("Images/checkmark.png",198, 96)
+checkmarkButton.x = 500
+checkmarkButton.y = 280
+checkmarkButton.isVisible = true
+
+-- Funtion: BlueButtonListener
+-- Input: touch listener
+-- output: none
+-- Description:when blue button os clicked, it will
+-- and make the blue button disappear
+local function BlueButtonListener(touch)
+    if (touch.phase == "began") then
+    	blueButton.isVisible = false
+    	redButton.isVisible = true
+    	textObject.isVisible = true
+    end	
+
+    if (touch.phase == "ended") then
+        blueButton.isVisible = true
+        redButton.isVisible = false
+        textObject.isVisible = false
+
+    end
+end    
+ 
+-- Funtion: redButtonListener
+-- Input: touch listener
+-- output: none
+-- Description:when red button os clicked, it will
+-- and make the red button disappear
+local function redButtonListener(touch)
+    if (touch.phase == "began") then
+    	redButton.isVisible = false
+    	blueButton.isVisible = true
+    	textObject.isVisible = true
+    end	
+
+    if (touch.phase == "ended") then
+        redButton.isVisible = true
+        blueButton.isVisible = false
+        textObject.isVisible = false
+
+    end
+end     
+
+-- Funtion: checkmarkButtonListener
+-- Input: touch listener
+-- output: none
+-- Description:when checkmark button os clicked, it will
+-- and make the checkmark button disappear
+local function checkmarkButtonListener(touch)
+    if (touch.phase == "began") then
+    	checkmarkButton.isVisible = false
+    	checkmarkButton.isVisible = true
+    	textObject.isVisible = true
+    end	
+
+    if (touch.phase == "ended") then
+        checkmarkButton.isVisible = true
+        checkmarkButton.isVisible = false
+        textObject.isVisible = false
+
+    end
+end     
+--add the respective listesteners to each object
+blueButton:addEventListener("touch", BlueButtonListener)  
