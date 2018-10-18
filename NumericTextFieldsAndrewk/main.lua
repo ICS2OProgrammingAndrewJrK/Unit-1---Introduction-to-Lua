a884cbead27f25f39ea10f81224d8d2384ac4b33
AndrewKuekam17 <43419144+AndrewKuekam17@users.noreply.github.com>
2018-10-18 14:58:27 -0400

    updated NumericTextFields

--- NumericTextFieldsAndrewk/main.lua
+++ NumericTextFieldsAndrewk/main.lua
@@ -7,9 +7,12 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 --sets the background colour 
-display.setDefault("background", 19/255, 123/255, 13/255)
+display.setDefault("background", 190/255, 123/255, 13/255)
 
+-------------------------------------------------------------------------------------
 --Local variables
+-------------------------------------------------------------------------------------
+
 
 --create local variables 
 local questionObject
@@ -23,7 +26,10 @@ local incorrectAnswer
 local correctAnswer
 local randomOperator
 
+------------------------------------------------------------------------------------
 -- local functions 
+------------------------------------------------------------------------------------
+
 local function AskQuestion()
 	--generate 2 random numbers between a max. and a min. number
 	randomOperator = math.random(0,3)
@@ -62,9 +68,6 @@ local function NumericFieldListener( event )
 	--User beging editing "numericfeild"
 	if ( event.phase == "began" ) then
 
-		--clear text field 
-		event.target.text = ""
-
 	elseif event.phase == "submitted" then
 
 		-- when the answer is submitted (enter key is pressed) set user input bto user's answer
@@ -79,37 +82,43 @@ local function NumericFieldListener( event )
 			correctObject.isVisible = false 
 			incorrectObject.isVisible = true
 			timer.performWithDelay(2000,HideCorrect)
-
-
 		end
+	    --clear text field 
+	    event.target.text = ""
 	end
 end
 
+----------------------------------------------------------------------------------------------------------------------------
 --object Creation 
+----------------------------------------------------------------------------------------------------------------------------
+
 
 -- display a question and sets the color 
 questionObject = display.newText("", display.contentWidth/3, display.contentHeight/2, nil, 50)
 questionObject:setTextColor(155/255, 42/255, 198/255)
 
 -- create the correct  text object and make it invisible
-correctObject = display.newText("correct Well done!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
-correctObject:setTextColor(155/255, 42/255, 198/255)
+correctObject = display.newText("correct, Well done!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
+correctObject:setTextColor(15/255, 42/255, 18/255)
 correctObject.isVisible = false
+
+-- create numeric field 
+numericField = native.newTextField(display.contentWidth/2, display.contentHeight/2, 150, 80)
+numericField.inputType = "number"
+
+
 
 -- create the incorrect  text object and make it invisible
 incorrectObject = display.newText("incorrect!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
 incorrectObject:setTextColor(15/255, 255/255, 18/255)
 incorrectObject.isVisible = false
 
--- create numeric field 
-
-numericField = native.newTextField(display.contentWidth/2, display.contentHeight/2, 150, 80)
-numericField.inputType = "number"
-
 --add the event listener fo the numeric field
 numericField:addEventListener( "userInput", NumericFieldListener)
 
+-----------------------------------------------------------------------------------------------------------------------------
 -- function calls
+-----------------------------------------------------------------------------------------------------------------------------
 
 -- call the functionto as the Question
 AskQuestion()
